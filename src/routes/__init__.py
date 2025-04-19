c60700cd3dd81ccf0d71317eeb0bc18394e0de99
Pénélope Peronnet <penelope.peronnet@student.griffith.ie>
2025-04-19 11:26:45 +0100

    feat: admin get employee profiles according to roles

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,10 +1,12 @@
 from src.routes.pages import pages_router
 from src.routes.users import users_router
+from src.routes.admin import admin_router
 # from src.routes.testimonials import testimonials_router
 
 ACTIVE_ROUTES = {
     "pages": pages_router,
     "users": users_router,
+    "admin": admin_router
     # "testimonials": testimonials_router,
 }
 
---
+++ src/routes/admin.py
@@ -0,0 +1,68 @@
+import uuid
+import logging
+from typing import List
+from fastapi import APIRouter,  HTTPException, Depends
+from src.pydantic_model.users import UserCreate, UserUpdate, UserResponse
+from src.database.models import User
+from src.database.models import UserRole
+
+from sqlalchemy.orm import Session
+from src.database import get_db
+
+
+logger = logging.getLogger(__name__)
+
+from src.database import SessionLocal, get_db
+
+admin_router = APIRouter(
+    prefix="/admin",
+    tags=["Admin"]
+)
+
+
+@admin_router.get("/get-user-by-role/{role}", response_model=List[UserResponse])
+async def get_all_users(role,
+    db: SessionLocal = Depends(get_db),
+    
+):
+    """
+    Get users according to their roles
+    """
+    
+    if role == "employee":
+        
+        users = db.query(User).filter(User.role== UserRole.EMPLOYEE).all()
+        
+    if role =="manager":
+        users = db.query(User).filter(User.role== UserRole.MANAGER).all()
+        
+    if role == "admin":
+        users = db.query(User).filter(User.role== UserRole.ADMIN).all()
+        
+    
+    logger.info(f"Found {len(users)} users")
+    if not users:
+        logger.warning(f"404 - No users found {users}")
+        raise HTTPException(status_code=404, detail="No users found")
+
+    return users
+
+@admin_router.get("/get-manager", response_model=List[UserResponse])
+async def get_all_users(
+    db: SessionLocal = Depends(get_db),
+    
+):
+    """
+    Get all managers
+    """
+    
+    
+    managers = db.query(User).filter(User.role== UserRole.MANAGER).all()
+    
+    logger.info(f"Found {len(managers)} users")
+    if not managers:
+        logger.warning(f"404 - No users found {managers}")
+        raise HTTPException(status_code=404, detail="No users found")
+  
+    
+    return managers
